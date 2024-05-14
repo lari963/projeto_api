89a1ca5cdb41f2b500ce22f20e2a24ed6a5a24c8
Larissa Helena <larissahelena963@gmail.com>
2024-05-14 13:01:04 -0300

    Salvando alterações

--- app.py
+++ app.py
@@ -1,59 +1,15 @@
-from flask import Flask, jsonify, request, funcoes # type: ignore
+from flask import Flask, render_template
+
+# Flask é um mine-framework (gerencia rotas)
 
 app = Flask(__name__)
 
-@app.route("/", methods=("GET",))
+@app.route("/")
 def index():
-    return jsonify({"status": 200, "message": "API Larissa helena"})
+    return render_template("index.html")
 
-@app.route("/aleatorios") #decorator de rota
-def aleatorios(): #função python 
-    import random
-    a = random.randint(49, 100)
-    return jsonify({"status": 200, "data": a}) # retorno
+from api import bp
+app.register_blueprint(bp)
 
-@app.route("/argumentos/<string:nome>")
-def argumentos(nome: str):
-    return jsonify({"status": 200, "data": nome})
-
-@app.route("/argumentos")
-def arg_implicito():
-    return jsonify({"status": 200, "data": request.args["nome"]})
-
-@app.route("/idades", methods=("GET",))
-def idades():
-    from random_data import pessoas
-    import funcoes 
-    num = funcoes.maior_de_50(pessoas)
-    return jsonify({"status": 200, "data": num})
-
-@app.route("/salario", methods = ("GET",))
-def salario():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.mais_2000(pessoas)
-    return jsonify( {"status": 200, "data": num} )
-
-@app.route("/maiores_salarios", methods = ("GET",))
-def dados():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.maior_salario(pessoas)
-    return jsonify( {"status": 200, "data": num} )
-
-@app.route("/salario_media", methods = ("GET",))
-def media_salarial():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.media_profissoes(pessoas)
-    return jsonify( {"status": 200, "data": num} )
-
-@app.route("/salario_idade", methods = ("GET",))
-def intervalo():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.maior_2000_sexo(pessoas)
-    return jsonify( {"status": 200, "data": num} )
-
-if __name__ == 'main':
-    app.run(debug=True)
+if __name__ == "_main_":
+    app.run(host="0.0.0.0")
